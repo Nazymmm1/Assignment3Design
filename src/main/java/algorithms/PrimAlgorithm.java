daa1155f5ba898630e5f97e2b8ff543c706817b6
Nazym <kurmanvaevanaz@gmail.com>
2025-10-25 21:33:28 +0500

    "Added Prim's algorithm"

--- src/main/java/algorithms/PrimAlgorithm.java
+++ src/main/java/algorithms/PrimAlgorithm.java
@@ -1,4 +1,48 @@
 package algorithms;
 
+import graph.Edge;
+import graph.Graph;
+import metrics.Metrics;
+
+import java.util.*;
+
 public class PrimAlgorithm {
+    public static List<Edge> run(Graph graph, Metrics metrics){
+        metrics.reset();
+        metrics.start();
+
+        List<Edge> mst= graph.getEdges();
+        Set<String> visited= new HashSet<>();
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(Edge::getWeight));
+
+        String start= graph.getVertices().get(0);
+        visited.add(start);
+
+        for(Edge edge: graph.getAdjacencyList().get(start)) {
+            priorityQueue.add(edge);
+        }
+        while (!priorityQueue.isEmpty()){
+            Edge edge= priorityQueue.poll();
+            metrics.addOperation();
+
+            if(visited.contains(edge.getTo())) continue;
+
+            mst.add(edge);
+            metrics.addCost(edge.getWeight());
+
+            visited.add(edge.getTo());
+
+            for(Edge next: graph.getAdjacencyList().get(edge.getTo())){
+                if(!visited.contains(next.getTo())){
+                    priorityQueue.add(next);
+                }
+            }
+
+        }
+
+        metrics.stop();
+        return mst;
+
+    }
+
 }
