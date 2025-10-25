335fd3ac377fccf5b50976e32dadda9eb0c788f7
Nazym <kurmanvaevanaz@gmail.com>
2025-10-25 13:39:18 +0500

    "Added metrics class for tracking algorithms' performance"

---
+++ src/main/java/metrics/Metrics.java
@@ -0,0 +1,36 @@
+package metrics;
+
+public class Metrics {
+    private int totalCost;
+    private int operationsCount;
+    private long startTime;
+    private long endTime;
+
+    public void start() {
+        startTime = System.nanoTime();
+    }
+
+    public void stop() {
+        endTime = System.nanoTime();
+    }
+
+    public double getExecutionTimeMs() {
+        return (endTime - startTime) / 1_000_000.0;
+    }
+
+    public void addOperation() {
+        operationsCount++;
+    }
+
+    public void addCost(int cost) {
+        totalCost += cost;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    public int getOperationsCount() {
+        return operationsCount;
+    }
+}
